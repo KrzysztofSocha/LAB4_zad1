acdbcb3cf99e109ebf298486ec728b7dfa74e86b
Krzysztof <krzysztof_socha@outlook.com>
2021-06-02 09:29:45 +0200

    LAB4 zadanie 1 commit 1

---
+++ WpfApp_LAB4_zadanie1_w61933/Student Edit.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace WpfApp_LAB4_zadanie1_w61933
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Student_Edit.xaml
+    /// </summary>
+    public partial class Student_Edit : Window
+    {
+        public Student student;
+        public Student_Edit(Student student = null)
+        {
+            if (student != null)
+            {
+                tbName.Text = student.imie;
+                tbSurname.Text = student.nazwisko;
+                tbID.Text = student.nrIndeksu.ToString();
+                tbDepart.Text = student.wydzial;
+            }
+            this.student = student ?? new Student();
+
+        }
+        
+        public Student_Edit()
+        {
+            InitializeComponent();
+        }
+
+        private void bOK_Click(object sender, RoutedEventArgs e)
+        {
+            //if (!Regex.IsMatch(tbName.Text, @"^\p{Lu} \p{Ll}{1,12}$")
+            //    || !Regex.IsMatch(tbSurname.Text, @"^\p{Lu} \p{Ll}{1,12}$")
+            //    || !Regex.IsMatch(tbDepart.Text, @"^\p{Lu} \p{Ll}{1,12}$")
+            //    || !Regex.IsMatch(tbID.Text, @"^[0-9]{4,10}$"))
+            //{
+            //    MessageBox.Show("Podano błędne dane!");
+            //    return;
+            //}
+            //else
+            //{
+                student.imie = tbName.Text;
+                student.nazwisko = tbSurname.Text;
+                student.nrIndeksu = int.Parse(tbID.Text);
+                student.wydzial = tbDepart.Text;
+
+                this.DialogResult = true;
+            
+
+        }
+    }
+}
---
+++ WpfApp_LAB4_zadanie1_w61933/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp_LAB4_zadanie1_w61933
+{
+    public class Student
+    {
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public int nrIndeksu { get; set; }
+        public string wydzial { get; set; }
+
+        public Student(string imie, string nazwisko, int nrIndeksu, string wydzial) {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.nrIndeksu = nrIndeksu;
+            this.wydzial = wydzial;
+        }
+        public Student() : this( "","",0,"")
+        {}
+        
+            
+        
+
+    }
+}
